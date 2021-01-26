15634524ba145aaa2eb517068484afe6d2fe38b7
bfallc <bfallc@artic.edu>
2021-01-26 15:37:52 -0500

    trying

--- checkboxes2/script.js
+++ checkboxes2/script.js
@@ -1,23 +1,28 @@
 /*jshint browser:true, undef: true, unused: true, jquery: true */
-
+console.log("hello");
 // store filter per group
 var filters = {};
 
+// quick search regex
+//search
+var qsRegex;
+
 // only used to dynamically generate items
-var data = {
-  brands: [ 'brand1', 'brand2', 'brand3', 'brand4' ],
-  productTypes: [ 'alpha', 'beta', 'gamma', 'delta' ],
-  colors: [ 'red', 'blue', 'green', 'yellow' ],
-  sizes: [ 'size8', 'size9', 'size10', 'size11' ],
-};
+// var data = {
+//   brands: [ 'brand1', 'brand2', 'brand3', 'brand4' ],
+//   productTypes: [ 'alpha', 'beta', 'gamma', 'delta' ],
+//   colors: [ 'red', 'blue', 'green', 'yellow' ],
+//   sizes: [ 'size8', 'size9', 'size10', 'size11' ],
+// };
 
 var $container = $('#container');
 
-createContent();
+// createContent();
 
 var $filterDisplay = $('#filter-display');
 
 $container.isotope();
+
 // do stuff when checkbox change
 $('#options').on( 'change', function( event ) {
   var checkbox = event.target;
@@ -40,12 +45,14 @@ $('#options').on( 'change', function( event ) {
   
   var comboFilter = getComboFilter();
   $container.isotope({ filter: comboFilter });
-  $filterDisplay.text( comboFilter );
+  // $filterDisplay.text( comboFilter );
 });
 
 
 
 function getComboFilter() {
+
+  
   var combo = [];
   for ( var prop in filters ) {
     var group = filters[ prop ];
@@ -73,31 +80,69 @@ function getComboFilter() {
   return comboFilter;
 }
 
+// var $container = $('#container').isotope({
+//   itemSelector: '.item',
+//   filter: function() {
+//     var $this = $(this);
+//     var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
+//     return searchResult;
+//   },
+// });
+
+//search
+// use value of search field to filter
+var $quicksearch = $('.quicksearch').keyup( debounce( function() {
+  qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+  $container.isotope({
+    itemSelector: '.item',
+    filter: function() {
+      var $this = $(this);
+      var searchResult = qsRegex ? $this.text().match( qsRegex ) : true;
+      return searchResult;
+    },
+  });
+}) );
+
+//search
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  threshold = threshold || 100;
+  return function debounced() {
+    clearTimeout( timeout );
+    var args = arguments;
+    var _this = this;
+    function delayed() {
+      fn.apply( _this, args );
+    }
+    timeout = setTimeout( delayed, threshold );
+  };
+}
 
 // helper made to make items
-function createContent() {
-  var brand, productType, color, size;
-  var items = '';
-  // dynamically create content
-  for (var i=0, len1 = data.brands.length; i < len1; i++) {
-    brand = data.brands[i];
-    for (var j=0, len2 = data.productTypes.length; j < len2; j++) {
-      productType = data.productTypes[j];
-        for (var l=0, len3 = data.colors.length; l < len3; l++) {
-        color = data.colors[l];
-        for (var k=0, len4 = data.sizes.length; k < len4; k++) {
-          size = data.sizes[k];
-          var itemHtml = '<div class="item ' + brand + ' ' +
-            productType + ' ' + color + ' ' + size + '">' +
-            '<p>' + brand + '</p>' +
-            '<p>' + productType + '</p>' +
-            '<p>' + size + '</p>' +
-            '</div>';
-            items += itemHtml;
-        }
-      }
-    }
-  }
+// function createContent() {
+//   var brand, productType, color, size;
+//   var items = '';
+//   // dynamically create content
+//   for (var i=0, len1 = data.brands.length; i < len1; i++) {
+//     brand = data.brands[i];
+//     for (var j=0, len2 = data.productTypes.length; j < len2; j++) {
+//       productType = data.productTypes[j];
+//         for (var l=0, len3 = data.colors.length; l < len3; l++) {
+//         color = data.colors[l];
+//         for (var k=0, len4 = data.sizes.length; k < len4; k++) {
+//           size = data.sizes[k];
+//           var itemHtml = '<div class="item ' + brand + ' ' +
+//             productType + ' ' + color + ' ' + size + '">' +
+//             '<p>' + brand + '</p>' +
+//             '<p>' + productType + '</p>' +
+//             '<p>' + size + '</p>' +
+//             '</div>';
+//             items += itemHtml;
+//         }
+//       }
+//     }
+//   }
 
-  $container.append( items );
-}
+//   $container.append( items );
+// }
